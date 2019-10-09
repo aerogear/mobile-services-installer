f46cf58d45d4dc71c5fb728f941f033581b5ced6
Wei Li <wei.li@redhat.com>
2019-10-09 16:05:38 +0100

    wait for pods to be ready and update readme

--- scripts/minishift.sh
+++ scripts/minishift.sh
@@ -4,12 +4,11 @@ cd "$(dirname "$0")" || exit 1
 
 minishift delete -f
 
-MINISHIFT_ENABLE_EXPERIMENTAL=y minishift start --openshift-version v3.11.0 \
---extra-clusterup-flags "--enable=*,service-catalog,automation-service-broker,template-service-broker" || exit 1
+minishift start || exit 1
 
 oc login -u system:admin
 
-ansible-playbook ../install-mobile-services.yml
+ansible-playbook ../install-mobile-services.yml -e registry_service_account_username=$REGISTRY_USERNAME -e registry_password=$REGISTRY_PASSWORD -e openshift_master_url="https://$(minishift ip)"
 
 export ROUTING_SUFFIX=$(minishift ip).nip.io
 export MINISHIFT=true
